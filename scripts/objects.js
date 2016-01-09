0eaca34c076f488b4cecfe4a8731538a60f5eeb5
Andy Malik <me@andymalik.com>
2016-01-09 14:54:36 -0800

    included script tags in HTML. Set up template in objects.js which allows for multiple authors in one project.

---
+++ scripts/objects.js
@@ -0,0 +1,58 @@
+var projects =  [
+  {
+    title: 'title',
+    category: 'cat',
+    description: 'description',
+    imgSpoiler: 'img link',
+    pubDate: 'mm-dd-yyy',
+    authors:[ //Array of authors, in case project has multiple authors
+      {
+        name: 'name',
+        link: 'url',
+      },
+      {
+        name: 'name2',
+        link: 'url2',
+      },
+    ],
+  },
+  {
+    title: 'title',
+    category: 'cat',
+    description: 'description',
+    imgSpoiler: 'img link',
+    pubDate: 'mm-dd-yyy',
+    authors:[ //Array of authors, in case project has multiple authors
+      {
+        name: 'name',
+        link: 'url',
+      },
+    ],
+  },
+  {
+    title: 'title',
+    category: 'cat',
+    description: 'description',
+    imgSpoiler: 'img link',
+    pubDate: 'mm-dd-yyy',
+    authors:[ //Array of authors, in case project has multiple authors
+      {
+        name: 'name',
+        link: 'url',
+      },
+    ],
+  },
+  {
+    title: 'title',
+    category: 'cat',
+    description: 'description',
+    imgSpoiler: 'img link',
+    pubDate: 'mm-dd-yyy',
+    authors:[ //Array of authors, in case project has multiple authors
+      {
+        name: 'name',
+        link: 'url',
+      },
+    ],
+  },
+];
