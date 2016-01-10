f72c975a596fc8a36e28fd79a40a8969ce683e1c
Andy Malik <me@andymalik.com>
2016-01-09 17:05:43 -0800

    Specified variables in object.js to improve debugging. Set up object constructor to match the keys from object.js.

--- scripts/objects.js
+++ scripts/objects.js
@@ -1,24 +1,74 @@
 var projectData =  [
   {
-    title: 'title',
-    category: 'cat',
-    description: 'description. html tags allowed.',
-    imgSpoiler: 'direct img link',
-    pubDate: 'Month Day, Year',
-    project-link: 'direct project link',
+    title: '0-title',
+    category: '0-cat',
+    description: '0-description. html tags allowed.',
+    imgSpoiler: '0-direct img link',
+    pubDate: 'October 20, 2010', //Do not add prefix (1st, 2nd, 3rd) to the numbers.
+    projectLink: '0-direct project link',
     authors:[ //Array of authors, in case project has multiple authors
       {
-        name: 'name',
-        link: 'url',
+        name: '0-0-name',
+        link: '0-0-url',
       },
       {
-        name: 'name2',
-        link: 'url2',
+        name: '0-1-name',
+        link: '0-1-url2',
+      }
+    ],
+  },
+  {
+    title: '1-title',
+    category: '1-cat',
+    description: '1-description. html tags allowed.',
+    imgSpoiler: '1-direct img link',
+    pubDate: 'October 20, 1999', //Do not add prefix (1st, 2nd, 3rd) to the numbers.
+    projectLink: '1-direct project link',
+    authors:[ //Array of authors, in case project has multiple authors
+      {
+        name: '1-0-name',
+        link: '1-0-url',
       },
       {
-        name: 'name3',
-        link: 'url3',
+        name: '1-1-name',
+        link: '1-1-url',
+      }
+    ],
+  },
+  {
+    title: '2-title',
+    category: '2-cat',
+    description: '2-description. html tags allowed.',
+    imgSpoiler: '2-direct img link',
+    pubDate: 'October 21, 2010', //Do not add prefix (1st, 2nd, 3rd) to the numbers.
+    projectLink: '2-direct project link',
+    authors:[ //Array of authors, in case project has multiple authors
+      {
+        name: '2-0-name',
+        link: '2-0-url',
       },
+      {
+        name: '2-1-name',
+        link: '2-1-url',
+      }
+    ],
+  },
+  {
+    title: '3-title',
+    category: '3-cat',
+    description: '3-description. html tags allowed.',
+    imgSpoiler: '3-direct img link',
+    pubDate: 'July 20, 2010', //Do not add prefix (1st, 2nd, 3rd) to the numbers.
+    projectLink: '3-direct project link',
+    authors:[ //Array of authors, in case project has multiple authors
+      {
+        name: '3-0-name',
+        link: '3-0-url',
+      },
+      {
+        name: '3-1-name2',
+        link: '3-1-url2',
+      }
     ],
   },
 ];
--- scripts/viewProjects.js
+++ scripts/viewProjects.js
@@ -1,3 +1,22 @@
 //This is going to read project-template-outline.html
 //translate it for template use
-//and plug in objects.js's variables accordingly
+//and plug in objects.js's variables accordingly.
+//Then it will perform any necessary actions before displaying the articles, like sorting them.
+
+var projects = [];
+
+function Project(itm){
+  this.title = itm.title;
+  this.category = itm.category;
+  this.description = itm.description;
+  this.imgSpoiler = itm.imgSpoiler;
+  this.pubDate = itm.pubDate
+  this.projectLink = itm.projectLink;
+  this.authors = itm.authors;
+}
+
+
+//Sort the project so that newest will be on top
+projectData.sort(function(a,b) {
+  return (new Date(b.pubDate)) - (new Date(a.pubDate));
+});
