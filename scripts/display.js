ba38a3d06c27b2be8a257c0758d7a5a0c5183a25
Andy Malik <me@andymalik.com>
2016-01-14 13:22:16 -0800

    Wrapped project.js and display.js inside IIFE. Replaced all forEach looped with .map() function.

--- scripts/display.js
+++ scripts/display.js
@@ -1,20 +1,22 @@
 //Create empty object to store functions
-var display={};
+(function(module) {
+  var display={};
 
-display.MainNavigation = function() {
-  $('#main-menu').on('click', '#tab', function() {
-    $('.placeholder').hide();
-    $('#' + $(this).data('filter') + "-placeholder").show();
-  });
-  $('#tab').data('filter', 'about').click();
-}
+  display.MainNavigation = function() {
+    $('#main-menu').on('click', '#tab', function() {
+      $('.placeholder').hide();
+      $('#' + $(this).data('filter') + "-placeholder").show();
+    });
+    $('#tab').data('filter', 'about').click();
+  }
 
-display.initIndexPage = function() {
-  console.log("display.initIndexPage start")
-  Project.all.forEach(function(ele, index, arr){
-    $('#project-placeholder').append(ele.toHtml(ele, index, arr));
-  });
+  display.initIndexPage = function() {
+    Project.all.map(function(ele, index, arr){
+      $('#project-placeholder').append(ele.toHtml(ele, index, arr));
+    });
 
-  display.MainNavigation();
-  console.log("display.initIndexPage finish; MainNavigation() should be working");
-}
+    display.MainNavigation();
+  }
+
+  module.display = display;
+})(window);
--- scripts/projects.js
+++ scripts/projects.js
@@ -1,91 +1,89 @@
-//This is going to read project-template-outline.html and author-template.html
-//translate it for template use
-//and plug in projData.js's variables accordingly.
-//Then it will perform any necessary actions before displaying the articles, like sorting them.
-//
-//After some trial and error, I couldn't figure out how to load the external html and run it through handlebars.
-//Settling down to including the template directly into index.html for now, as to move on with the project.
-//Will revisit later.
+/**********************************************************************************\
+|  This is going to read project-template-outline.html and author-template.html    |
+|  translate it for template use and plug in projData.js's variables accordingly.  |
+|  Then it will perform any necessary actions before displaying the articles,      |
+|  like sorting them.                                                              |
+|                                                                                  |
+|  After some trial and error, I couldn't figure out how to load the external html |
+|  and run it through handlebars. Settling down to including the template directly |
+|  into index.html for now, as to move on with the project. Will revisit later.    |
+\**********************************************************************************/
 
-
-//I'm reading up on the Class-06 Pair Assignment's TODOs to help
-//educate me more on the code I need to write for this Class-06 Portfolio User Stories
-//I will be using similar naming convensions so I can learn while reading and re-typing the code
-
-function Project(itm){
-  this.title = itm.title;
-  this.category = itm.category;
-  this.description = itm.description;
-  this.imgSpoiler = itm.imgSpoiler;
-  this.pubDate = itm.pubDate
-  this.projectLink = itm.projectLink;
-  this.authorPlaceholder = '';
-  this.authors = itm.authors;
-}
-
-//Instead of a global `projects = []` array, let's track this list directly on the constructor function
-//*******  I'm confused on the reasoning behind this change. *********//
-//var projects = [];
-Project.all = [];
-
-
-Project.prototype.toHtml = function(ele, index, arr) {
-  var finAuthors = '';   //delcare local variables
-  var finProj = '';
-  var authorTemplate = $('#authTemp').html();
-  var projectTemplate = $('#projTemp').html();
-  var dataSource = this;
-  var compAuth = Handlebars.compile(authorTemplate);
-  var compProj = Handlebars.compile(projectTemplate);
-  if(index === 0){  //Place the heading before the first item only
-    finProj = "<h1>Projects I've made</h1>"
+(function(module) {
+  function Project(itm){
+    this.title = itm.title;
+    this.category = itm.category;
+    this.description = itm.description;
+    this.imgSpoiler = itm.imgSpoiler;
+    this.pubDate = itm.pubDate
+    this.projectLink = itm.projectLink;
+    this.authorPlaceholder = '';
+    this.authors = itm.authors;
   }
-  dataSource.authors.forEach(function(ele, index, arr){  //Compile for each author.
-    finAuthors += compAuth(ele);
-    if(index !== arr.length-1){
-      finAuthors += " and "; //Place " and " after each author that isn't the last one. We don't need to do anything for the last author.
-    };
-  });
-  this.authorPlaceholder = finAuthors;
-  if(index !== arr.length-1){
-    finProj += compProj(dataSource);
-  } else {
-    finProj += compProj(dataSource);
-  }
-  return finProj;
-}
 
-Project.loadAll = function(projData) {
-  projData.sort(function(a,b) {
-    return (new Date(b.pubDate)) - (new Date(a.pubDate));
-  });
-  projData.forEach(function(ele) {
-    Project.all.push(new Project(ele));
-  });
-}
+  //Instead of a global `projects = []` array, let's track this list directly on the constructor function
+  //*******  I'm confused on the reasoning behind this change. *********//
+  //var projects = [];
+  Project.all = [];
 
-Project.fetchAll = function() {
-  $.ajax({
-    dataType: "json",
-    url: "/data/projData.JSON",
-    success: function(data, status, xhr){
-      if(localStorage.etag === xhr.getResponseHeader('ETag')){
-        Project.loadAll(JSON.parse(localStorage.projData));
-        console.log("Inside Fetch All Function: ETag exists");
-        display.initIndexPage();
-      } else {
-        localStorage.etag = xhr.getResponseHeader('ETag');
-        $.getJSON('/data/projData.JSON', function(projData){
-          Project.loadAll(projData);
-          localStorage.projData = JSON.stringify(projData);
-          console.log("Inside Fetch All Function: ETag does not exist");
-          display.initIndexPage();
-        });
-      }
-    },
-    error: function (request, status, error) {
-        alert(error);
-        console.log(error);
+
+  Project.prototype.toHtml = function(ele, index, arr) {
+    var finAuthors = '';   //delcare local variables
+    var finProj = '';
+    var authorTemplate = $('#authTemp').html();
+    var projectTemplate = $('#projTemp').html();
+    var dataSource = this;
+    var compAuth = Handlebars.compile(authorTemplate);
+    var compProj = Handlebars.compile(projectTemplate);
+    if(index === 0){  //Place the heading before the first item only
+      finProj = "<h1>Projects I've made</h1>"
     }
-  });
-}
+    dataSource.authors.map(function(ele, index, arr){  //Compile for each author.
+      finAuthors += compAuth(ele);
+      if(index !== arr.length-1){
+        finAuthors += " and "; //Place " and " after each author that isn't the last one. We don't need to do anything for the last author.
+      };
+    });
+    this.authorPlaceholder = finAuthors;
+    if(index !== arr.length-1){
+      finProj += compProj(dataSource);
+    } else {
+      finProj += compProj(dataSource);
+    }
+    return finProj;
+  }
+
+  Project.loadAll = function(projData) {
+    projData.sort(function(a,b) {
+      return (new Date(b.pubDate)) - (new Date(a.pubDate));
+    });
+    projData.map(function(ele) {
+      Project.all.push(new Project(ele));
+    });
+  }
+
+  Project.fetchAll = function() {
+    $.ajax({
+      dataType: "json",
+      url: "/data/projData.JSON",
+      success: function(data, status, xhr){
+        if(localStorage.etag === xhr.getResponseHeader('ETag')){
+          Project.loadAll(JSON.parse(localStorage.projData));
+          display.initIndexPage();
+        } else {
+          localStorage.etag = xhr.getResponseHeader('ETag');
+          $.getJSON('/data/projData.JSON', function(projData){
+            Project.loadAll(projData);
+            localStorage.projData = JSON.stringify(projData);
+            display.initIndexPage();
+          });
+        }
+      },
+      error: function (request, status, error) {
+          alert(error);
+          console.log(error);
+      }
+    });
+  }
+  module.Project = Project;
+})(window);
