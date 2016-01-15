3655608401049428439fc00d60a33fa8722f4834
Andy Malik <me@andymalik.com>
2016-01-14 18:56:11 -0800

    Menu button flips to an X. Going to learn more about CSS Animations to improve the sidebar.

--- scripts/display.js
+++ scripts/display.js
@@ -10,12 +10,26 @@
     $('#tab').data('filter', 'about').click();
   }
 
+  display.showSidebar = function() {
+    $('#flip-container').on('click', function() {
+      document.querySelector('#flip-container').classList.toggle("flip");
+      /*
+      $('#main-menu').toggle('slide', { direction: 'right' }, 1000);
+      $('#MoveMe').toggle('slide', { direction: 'left' }, 1000);
+      $('.mobilemenu').animate({opacity: "toggle"}, 1000);
+      $('.closemobilemenu').animate({opacity: "toggle"}, 1000);
+      */
+      // $('mobilemenu').toggle('slide', { direction: 'left' }, 1000);
+    });
+  }
+
   display.initIndexPage = function() {
     Project.all.map(function(ele, index, arr){
       $('#project-placeholder').append(ele.toHtml(ele, index, arr));
     });
 
     display.MainNavigation();
+    display.showSidebar();
   }
 
   module.display = display;
