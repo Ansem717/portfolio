26c4673fffb768c178cfd7f0d15a7fb7748f5372
Andy Malik <me@andymalik.com>
2016-01-23 20:59:44 -0800

    I thought I already installed an ESLinter, but it seems I did not. Added and fixed my code to fit the rules, but then I added some exceptions, because it didn't like my inline comments and stuff. Kind of annoying, actually. So, I didn't really go into using Page.js to middleware specific links, since my projects are not on my local files, it felt like too much uncessary work. I did middleware the About and Project pages, but I that code is not new. If this is not what you would like, lets talk. Lastly, I cleaned out alot of the code, and removed comments to myself, as well as chunks of code commented out. Linter and Refractoring is done '

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,10 +1,14 @@
+/* global $ Handlebars localStorage display alert window:true*/
+/* eslint no-alert: 0, require-jsdoc: 0, no-inline-comments: 0*/
+/* the above is instructions to the linter */
+
 (function(module) {
-  function Project(itm){
+  function Project(itm) {
     this.title = itm.title;
     this.category = itm.category;
     this.description = itm.description;
     this.imgSpoiler = itm.imgSpoiler;
-    this.pubDate = itm.pubDate
+    this.pubDate = itm.pubDate;
     this.projectLink = itm.projectLink;
     this.authorPlaceholder = '';
     this.authors = itm.authors;
@@ -12,60 +16,60 @@
 
   Project.all = [];
 
-  Project.prototype.toHtml = function(ele, index, arr) {
-    var finAuthors = '';   //delcare local variables
+  Project.prototype.toHtml = function(ele, index) {
+    var finAuthors = '';   // delcare local variables
     var finProj = '';
     var authorTemplate = $('#authTemp').html();
     var projectTemplate = $('#projTemp').html();
     var dataSource = this;
     var compAuth = Handlebars.compile(authorTemplate);
     var compProj = Handlebars.compile(projectTemplate);
-    if(index === 0){  //Place the heading before the first item only
-      finProj = "<h1>Projects I've made</h1>"
+    if (index === 0) {  // Place the heading before the first item only
+      finProj = '<h1>Projects I\'ve made</h1>';
     }
-    dataSource.authors.map(function(ele, index, arr){  //Compile for each author.
+    dataSource.authors.map(function(ele, index, arr) {  // Compile for each author.
       finAuthors += compAuth(ele);
-      if(index !== arr.length-1){
-        finAuthors += " and "; //Place " and " after each author that isn't the last one. We don't need to do anything for the last author.
-      };
+      if (index !== arr.length - 1) {
+        finAuthors += ' and '; // Place ' and ' after each author that isn't the last one. We don't need to do anything for the last author.
+      }
     });
     this.authorPlaceholder = finAuthors;
     finProj += compProj(dataSource);
     return finProj;
-  }
+  };
 
   Project.loadAll = function(projData) {
-    projData.sort(function(a,b) {
+    projData.sort(function(a, b) {
       return (new Date(b.pubDate)) - (new Date(a.pubDate));
-    });
-    projData.map(function(ele) {
+    })
+    .map(function(ele) {
       Project.all.push(new Project(ele));
     });
-  }
+  };
 
   Project.fetchAll = function() {
     $.ajax({
-      dataType: "json",
-      url: "/data/projData.JSON",
-      success: function(data, status, xhr){
-        if(localStorage.etag === xhr.getResponseHeader('ETag')){
+      dataType: 'json',
+      url: '/data/projData.JSON',
+      success: function(data, status, xhr) {
+        if (localStorage.etag === xhr.getResponseHeader('ETag')) {
           Project.loadAll(JSON.parse(localStorage.projData));
           display.initIndexPage();
         } else {
           localStorage.etag = xhr.getResponseHeader('ETag');
-          $.getJSON('/data/projData.JSON', function(projData){
+          $.getJSON('/data/projData.JSON', function(projData) {
             Project.loadAll(projData);
             localStorage.projData = JSON.stringify(projData);
             display.initIndexPage();
           });
         }
       },
-      error: function (request, status, error) {
-          alert(error);
-          console.log(error);
+      error: function(request, status, error) {
+        alert(error);
+        console.log(error);
       }
     });
-  }
+  };
 
   module.Project = Project;
 })(window);
